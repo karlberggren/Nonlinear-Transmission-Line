34092bdf31f624626c5c873aae7e673dcba069dc
Karl K. Berggren <berggren@mit.edu>
2020-02-28 17:51:01 -0500

    start weekend work

--- nonlinsim.py
+++ nonlinsim.py
@@ -29,10 +29,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from scipy.special import erf as erf
+from scipy import constants
 
 # These are physical constants... they're gonna be globals
-constants = {"mu_o": 1.26e-7,
-             "eps_o": 8.85e-12}
+c = constants.speed_of_light
+Zo = np.sqrt(constants.mu_0/constants.epsilon_0)
 
 """
 Create some helper functions, to make it easier to create some generic
@@ -40,7 +41,7 @@ inputs.  Of course, user can always make themselves a custom function
 to pass as input.
 """
 
-def gaussian(amplitude, center, sigma, offset = 0):
+def gaussian(amplitude, t_offset, sigma, offset = 0):
     """
     generic gaussian function, for creating gaussian input pulse
     """
@@ -49,21 +50,21 @@ def gaussian(amplitude, center, sigma, offset = 0):
 
     return v_IN
 
-def sinusoid(amplitude, frequency, phase = 0, offset = 0):
+def sinusoid(amplitude, frequency, t_offset = 0, offset = 0):
     """
     generic sinusoidal function, for creating sinusoidal input
     """
     def v_IN(t):
-        return amplitude * np.cos(2*np.pi*frequency + phase) + offset
+        return amplitude * np.cos(2*np.pi*frequency*(t - t_offset)) + offset
 
     return v_IN
 
-def step(amplitude, time, width, offset = 0):
+def step(amplitude, t_offset, width, offset = 0):
     """
     generic sigmoid function
     """
     def v_IN(t):
-        return amplitude*(1/2+1/2*erf((t-time)/width)) + offset
+        return amplitude*(1/2+1/2*erf((t-t_offset)/width)) + offset
 
     return v_IN
 
@@ -95,21 +96,17 @@ def simulate(v_IN, sim_params, params):
 
         specify nonlinear dependence of mu on current.  
         """
-        mu_r = params["mu_r"]*(1 + 2*params["alpha"]*current**2 +
+        return params["mu_r"]*(1 + 2*params["alpha"]*current**2 +
                                4*params["beta"]*current**4)
-        return  mu_r * constants["mu_o"]
 
     def mu_eff(current):
         """
         it turns out the i dL/dt effect on voltage can be folded in by
         using an "effective mu" (see notes of 2020-01-02).
         """
-        mu_eff = params["mu_r"] * \
-                 constants["mu_o"] *(1 +
-                                     3*params["alpha"]*current**2 +
-                                     5*params["beta"]*current**4)
-        return mu_eff
-                                                      
+        return params["mu_r"] * (1 +
+                                 3*params["alpha"]*current**2 +
+                                 5*params["beta"]*current**4)
 
     t = 0.0  # tracks total simulation time
 
@@ -159,9 +156,9 @@ def simulate(v_IN, sim_params, params):
         
         newi = []  # i(t+dt)
         newv = []  # v(t+dt)
-        newhotspot = hotspot[:]
+        newhotspot = hotspot[:]  # FIXME probably slow
         
-        eps = params["eps_r"]*constants["eps_o"]
+        eps = params["eps_r"]
 
         # deal with left boundary
         if left_boundary['type'] == 'source':
@@ -205,7 +202,10 @@ def simulate(v_IN, sim_params, params):
 
             # calculate resistivity so that cutoff frequency will be as specified
             
-            iR = -dv / (sim_params["rho"] * dx)  # current in resistor
+            if sim_params['rho']:
+                iR = -dv / (sim_params["rho"] * dx)  # current in resistor
+            else:
+                iR = 0
             di = i[n] + iR - i[n+1]  # current in capacitor
            
             # first do routine calculations, will overwrite if needed
@@ -221,6 +221,7 @@ def simulate(v_IN, sim_params, params):
                     pointing in direction current was already directed
                     """
                     newi[n] = np.sign(newi[n])*params["ihs"]
+                    hotspot_timer = 0
             else:
                 # at a hotspot, check for healing
                 old_dv = v[n] - v[n-1]
@@ -230,9 +231,11 @@ def simulate(v_IN, sim_params, params):
                    (np.sign(new_dv) != np.sign(old_dv)):
                     # heal hotspot
                     newhotspot[n] = False
+                    hotspot_timer = None
                 else:
                     # not healed, overwrite previous calc and preserve
                     # current (i.e. i_HS).
+                    hotspot_timer += dt
                     newi[n] = i[n]
                     
         # deal with right boundary
@@ -255,7 +258,10 @@ def simulate(v_IN, sim_params, params):
             """
             # I've been having troubles when very large gradients are present.
             dv = v[-2] - v[-1]
-            iR = - dv / (sim_params["rho"]*dx)
+            if sim_params['rho']:
+                iR = - dv / (sim_params["rho"]*dx)
+            else:
+                iR = 0
             di = i[-1] + iR - v[-1]/(Rload + .01)
             newv.append(v[-1] + di * dt / eps / dx)
             newi.append(dv * dt / mus / dx + i[-1])
@@ -267,15 +273,20 @@ def simulate(v_IN, sim_params, params):
         """
         calculate total energy stored in transmission line
         """
-        eps = params["eps_r"]*constants["eps_o"]
+        Co = params["eps_r"] * dx
         nrg = 0
-        Lo = constants["mu_o"]*params["mu_r"]*dx
+        Lo = params["mu_r"] * dx
         α = params["alpha"]
         β = params["beta"]
         for n,io in enumerate(i):
-            if hotspot[n] is not True :
-                nrg += Lo * (0.5*io**2 + 2/3*α*io**3 + 1/4*α*io**4 + β*io**5)
-            nrg += 0.5 * eps * dx * v[n]**2  # hotspot only replaces inductor, not cap
+            # sum all inductors not hotspots
+            if not hotspot[n]:
+                nrg += Lo * (0.5*io**2 +
+                             2/3*α*io**3 +
+                             1/4*α*io**4 +
+                             β*io**5)
+            # hotspot only replaces inductor, not cap
+            nrg += 0.5 * Co * v[n]**2
         return nrg
 
     def power(Vin, i, v, hotspot) :
@@ -294,14 +305,16 @@ def simulate(v_IN, sim_params, params):
             term_power = 0
 
         loss = 0
-        for n in range(1,len(i)):
-            loss += (v[n] - v[n-1])**2/(sim_params["rho"]*dx)
+        Rloss = sim_params["rho"]*dx
+        if Rloss != 0:
+            for n in range(1,len(i)):
+                loss += (v[n] - v[n-1])**2 / Rloss
             
         return -i[0]*Vin + \
             params["RIN"]*i[0]**2 + \
             loss + \
             term_power + \
-            hspower
+            hspower  # lost in hotspot
 
     left_boundary = {}
     right_boundary = {}
@@ -311,7 +324,7 @@ def simulate(v_IN, sim_params, params):
     right_boundary['load impedance'] = params['RL']
 
     detailed_balance = []
-    
+
     while t < duration:
         left_boundary["source strength"] = v_IN(t)
         valid_step = False
@@ -322,6 +335,9 @@ def simulate(v_IN, sim_params, params):
                                   v,
                                   hotspot)
         while (valid_step != True):
+            if args.debug :
+                #print(f"Time is {t}")
+                pass
             
             # Where simulation occurs
             temp_i,temp_v,temp_hotspot  = timestep(i,
@@ -332,51 +348,49 @@ def simulate(v_IN, sim_params, params):
                                                    dt)
 
             energy_after_step = energy(temp_i, temp_v, temp_hotspot)
-            if args.debug :
-                print(f"energy after step: {energy_after_step:.3}")
-                pass
 
             step_nrg_gain = power_during_step*dt + \
                             energy_after_step - \
                             energy_before_step
 
-            if args.debug :
-                print(f"step_nrg_gain: {step_nrg_gain:.3}")
-                pass
-
-            # now we have to check that energy is reasonably close to conserved, if not take a smaller
-            # timestep.
+            # now we have to check that energy is reasonably close to
+            # conserved, if not take a smaller timestep.
             nrg_gain_max = sim_params["nrg_gain_max"]
             nrg_gain_min = sim_params["nrg_gain_min"]
+            """
+            need to check np.abs(energy_after_step) > 0 so we don't get 
+            divide by zero error some weird thing with low energy cases. 
+            Also need to make sure a brand new hotspot wasn't just created 
+            (energy isn't conserved by construction across a hotspot 
+            creation event).
+            """
+            # 1 hotspot now, but wasn't last round
+            new_hotspot = bool(sum(temp_hotspot) - sum(hotspot))
 
-            # need to check np.abs(energy_after_step) > 0 so we don't get divide by zero error
-            # some weird thing with low energy cases.  Also need to make sure a brand new hotspot
-            # wasn't just created (energy isn't conserved by construction across a hotspot creation event).
-            new_hotspot = bool(sum(temp_hotspot) - sum(hotspot))  # 1 hotspot now, but wasn't last round
-            if new_hotspot == False  and \
-               np.abs(energy_after_step) > 1e-25 and \
-               (np.abs(step_nrg_gain/energy_after_step) > nrg_gain_max):  
-                # moving too quickly
-                # look closely at various parameters (power, energy, changes) in this period
-                
-                valid_step = False
-                dt = dt/2
-                if args.verbose :
-                    print(f"Energy change too large, dt = {dt:.3}")
-            else :  # no need to repeat, not moving too quickly (might be too slow?)
+            # hotspot breaks nrg conservation
+            if sim_params["adaptive_time"] and new_hotspot == False:
+                if np.abs(energy_after_step) > 1e-25 and \
+                   np.abs(step_nrg_gain/energy_after_step) > nrg_gain_max:
+                    # moving too quickly
+                    valid_step = False
+                    dt = dt/2
+                    if args.verbose :
+                        print(f"Energy change too large, dt = {dt:.3}")
+            else :  # not moving too quickly, or not adaptive, or hotspot
                 valid_step = True
                 i = temp_i
                 v = temp_v
                 hotspot = temp_hotspot
                 t += dt
                 frame_timer += dt
-                if np.abs(energy_after_step) > 0 and \
+                if sim_params["adaptive_time"] and \
+                   np.abs(energy_after_step) > 0 and \
                    np.abs(step_nrg_gain/energy_after_step) < nrg_gain_min:
-                    # moving too slowly
                     dt = 2*dt
                     if args.verbose :
                         print(f"Energy change too small, dt = {dt:.3}")
-
+                
+        # step completed successfully
         detailed_balance.append((t,step_nrg_gain))
         
         if frame_timer > frame_duration:
@@ -408,21 +422,21 @@ if __name__ == '__main__':
     parser.add_argument('--frames', default = 10, type = int,
                         help = 'number of frames to output')
     parser.add_argument('--Rload', default = 'match', 
-                        help = 'load impedance [ohms] or "match" to match impedance')
+                        help = 'load impedance [Ω] or "match" to match impedance')
     parser.add_argument('--Rin', default = 'match',
-                        help = 'output impedance of source [ohms] or "match" to match impedance')
+                        help = 'output impedance of source [Ω] or "match" to match impedance')
     parser.add_argument('--alpha', default = '0', type = float,
-                        help = 'quadratic term in nonlinearity [A**(-2)]')
+                        help = 'quadratic term in nonlinearity [A⁻²]')
     parser.add_argument('--beta', default = '0', type = float,
-                        help = 'quartic term in nonlinearity [A**(-4)]')
+                        help = 'quartic term in nonlinearity [A⁻⁴]')
     parser.add_argument('--source', default = 'gaussian',
                         help = 'source type: gaussian, sinusoid, or step')
     parser.add_argument('--amplitude', default = .001, type = float,
-                        help = 'amplitude of source')
-    parser.add_argument('--phase', default = 1.0, type = float,
-                        help = 'time offset from zero of source')
+                        help = 'amplitude of source [A]')
+    parser.add_argument('--t_offset', default = 1.0, type = float,
+                        help = 'time offset from zero of source [s]')
     parser.add_argument('--sigma', default = 0.4, type = float,
-                        help = 'standard deviation of gaussian, or period of sinusoidal source')
+                        help = 'standard deviation of gaussian, or period of sinusoidal source [s]')
     parser.add_argument('--offset', default = 0.0, type = float,
                         help = 'DC offset value of source')
     parser.add_argument('-p', '--plot', action = 'store_true',
@@ -452,63 +466,66 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
     if args.Rin == 'match':
-        args.Rin = np.sqrt(args.mur*constants['mu_o']/args.epsr/constants['eps_o'])
-        if args.debug:
-            print(f"Rin calculated to be {args.Rin}")
+        args.Rin = np.sqrt(args.mur/args.epsr)
     if args.Rload == 'match':
-        args.Rload = np.sqrt(args.mur*constants['mu_o']/args.epsr/constants['eps_o'])
-        if args.debug:
-            print(f"Rload calculated to be {args.Rload}")
+        args.Rload = np.sqrt(args.mur/args.epsr)
 
     # for parameters that are about the physical system primarily
     params = {"mu_r": args.mur,
               "eps_r": args.epsr,
-              "RIN": float(args.Rin),
-              "RL": float(args.Rload),
+              "RIN": args.Rin,
+              "RL": args.Rload,
               "alpha": args.alpha,
               "beta": args.beta,
               "ic": args.ic,
               "ihs": args.ihs,
               "hotspot_location": args.hsloc,
               "bias": args.bias,
-              "length": args.length,
-              "duration": args.duration
+              "length": 1., # always use length 1, and normalize
+              # convert duration to sim units by * c/L
+              "duration": args.duration * c / args.length
     }
 
     sourcelist = {'gaussian': gaussian(args.amplitude,
-                                       args.phase,
-                                       args.sigma,
+                                       args.t_offset * c / args.length,
+                                       args.sigma * c / args.length,
                                        args.offset),
                   'step': step(args.amplitude,
-                               args.phase,
-                               args.sigma,
+                               args.t_offset * c / args.length,
+                               args.sigma * c / args.length,
                                args.offset),
                   'sinusoid': sinusoid(args.amplitude,
-                                       args.phase,
-                                       1/args.sigma,
+                                       args.t_offset * c / args.length,
+                                       1/(args.sigma * c / args.length),
                                        args.offset)
     }
 
     # for parameters that describe the source
     source_params = {"type": args.source,
                      "amplitude": args.amplitude,
-                     "phase": args.phase,
+                     "t_offset": args.t_offset,
                      "sigma": args.sigma,
                      "offset": args.offset}
     
     # for parameters that are about the numerics primarily
-    sim_params = {"dx": args.dx,
-                  "dt": args.dt,
+    sim_params = {"dx": args.dx/args.length,
+                  "dt": args.dt/args.duration,
                   "numframes": args.frames,
-                  "nrg_gain_max" : 1e-7,
-                  "nrg_gain_min": 1e-9,
-                  "Vretrap": args.ihs*args.Rsheet*.1,
-                  "dv_cap": args.dx*1e6,  # set max grad 1 MV/m
-                  "di_cap": args.dx*1e6,  # just guessing for current grad
-                  "fcut": args.fcut
+                  "nrg_gain_max" : 1e-6,
+                  "nrg_gain_min": 1e-8,
+                  "Vretrap": args.ihs * args.Rsheet / Zo * .1,
+                  "fcut": args.fcut * args.length / c,
+                  "adaptive_time": args.adaptt
                   }
-    sim_params["rho"] = 2*np.pi*constants["mu_o"]*params["mu_r"]*sim_params["fcut"]
+    # resistance per unit length.  all the componetns have already
+    # been converted to sim units, so shouldn't require further conversion
 
+    # sim_params["rho"] = 2*np.pi*params["mu_r"]*sim_params["fcut"]
+    sim_params["rho"] = 0
+
+    if args.debug:
+        print(params)
+        print(sim_params)
 
 
     frames_out,detailed_balance  = simulate(sourcelist[args.source],
